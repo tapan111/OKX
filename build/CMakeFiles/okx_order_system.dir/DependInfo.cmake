479f141c5a2841063383ddf038c7247a40d12212
Tapan Karangiya <olofury@gmail.com>
2024-10-20 23:40:29 +0530

    added intial code

---
+++ build/CMakeFiles/okx_order_system.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tapankarangiya/Desktop/OKX/OKXOrderSystem.cpp" "CMakeFiles/okx_order_system.dir/OKXOrderSystem.cpp.o" "gcc" "CMakeFiles/okx_order_system.dir/OKXOrderSystem.cpp.o.d"
+  "/Users/tapankarangiya/Desktop/OKX/okx_client.cpp" "CMakeFiles/okx_order_system.dir/okx_client.cpp.o" "gcc" "CMakeFiles/okx_order_system.dir/okx_client.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/okx_order_system.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for okx_order_system.
